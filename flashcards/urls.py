a7dc2636161153f7af002dbae15cb55b0a03cb64
Manorath Bhatt <bhattmani10@gmail.com>
2026-03-07 23:50:43 +0545

    feat:add 'continue as guest' option in login UI

--- flashcards/urls.py
+++ flashcards/urls.py
@@ -9,4 +9,5 @@ urlpatterns = [
     path('update/', views.update_status, name='update_status'),
     path('api/card/', views.get_card, name='get_card'),
     path('api/stats/', views.get_stats, name='get_stats'),
+    path('api/all_cards/', views.all_cards, name='all_cards'),
 ]
